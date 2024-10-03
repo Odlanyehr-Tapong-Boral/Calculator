9a2c5dd4f1e91efd5cecca1eb79fa6f523eda164
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-10-03 16:40:37 +0800

    calculator3

--- calculator.py
+++ calculator.py
@@ -6,4 +6,8 @@ root.title("Calculator")
 root.geometry("570x600+100+200")
 root.resizable(False,False)
 root.configure(bg="#17161b")
+
+
+Button(root,text="C",width=5,height=1,font=("arial",30,"bold"), bd=1,fg="#fff",bg="#3697f5",command=lambda: clear()).place(x=10,y=100)
+
 root.mainloop()
