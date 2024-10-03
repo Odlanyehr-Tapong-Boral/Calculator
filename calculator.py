a5189182d210111e83e35c1303077a2406619f74
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-10-03 18:17:57 +0800

    calculator6

--- calculator.py
+++ calculator.py
@@ -13,6 +13,25 @@ def show(value):
         global equation
         equation+=value
         label_result.config(text=equation)
+        
+def clear():
+        global equation
+        equation = ""
+        label_result.config(text=equation) 
+        
+def calculate():
+        global equation
+        result =""
+        if equation !="":
+           try:     
+                result= eval(equation)
+           except:
+                result ="error"                   
+                equation = ""
+        label_result.config(text=result)
+
+label_result=Label(root,width=25,height=2,text="",font=("arial",30))
+label_result.pack()        
 
 
 
